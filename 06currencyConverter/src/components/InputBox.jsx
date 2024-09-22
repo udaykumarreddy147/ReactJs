bfd028c3fc32f30b02e689acb50e8e8c475e6af4
udaykumarreddy147 <mvpuday147@gmail.com>
2024-09-22 17:11:47 +0530

    new update

--- 06currencyConverter/src/components/InputBox.jsx
+++ 06currencyConverter/src/components/InputBox.jsx
@@ -10,7 +10,8 @@ function InputBox({
  
 
   return (
-      <div className={`bg-white p-3 rounded-lg text-sm flex `}>
+      <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
+
           <div className="w-1/2">
               <label  className="text-black/40 mb-2 inline-block">
                   {label}
@@ -22,18 +23,19 @@ function InputBox({
                   placeholder="Amount"
               />
           </div>
+
           <div className="w-1/2 flex flex-wrap justify-end text-right">
+
               <p className="text-black/40 mb-2 w-full">Currency Type</p>
-              <select
-                  className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
-                  
-              >
+
+              <select className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none" >
                   
                       <option value="usd">
                           usd
                       </option>
               
               </select>
+
           </div>
       </div>
   );
